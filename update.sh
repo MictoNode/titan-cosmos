5e7ece949cfe9d32711af316a260f08baf8d0e86
MictoNode <94179917+MictoNode@users.noreply.github.com>
2024-08-07 13:57:08 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,14 +7,10 @@ cd ~ || exit
 systemctl stop titand
 
 # Back up the .titan directory. If something goes wrong in the middle, you can use the backup to restore and then execute again.
-mv ~/.titan ~/titan_bak
+mv ~/.titan ~/titan_bak_08_07
 
 # Copy directory and other information to the new path
-rsync -av --exclude "data" ~/titan_bak/* ~/.titan
-
-# Delete existing executable program
-# shellcheck disable=SC2046
-rm $(which titand)
+rsync -av --exclude "data" ~/titan_bak_08_07/* ~/.titan
 
 # Download new genesis file
 wget -P ~/. https://raw.githubusercontent.com/Titannet-dao/titan-chain/main/genesis/genesis.json
@@ -30,21 +26,5 @@ echo '{
   "step": 0
 }' > ~/.titan/data/priv_validator_state.json
 
-# Update config/client.toml chain-id
-echo '# This is a TOML config file.
-# For more information, see https://github.com/toml-lang/toml
 
-###############################################################################
-###                           Client Configuration                            ###
-###############################################################################
-
-# The network chain ID
-chain-id = "titan-test-2"
-# The keyring s backend, where the keys are stored (os|file|kwallet|pass|test|memory)
-keyring-backend = "os"
-# CLI output format (text|json)
-output = "text"
-# <host>:<port> to Tendermint RPC interface for this chain
-node = "tcp://localhost:29657"
-# Transaction broadcasting mode (sync|async)
-broadcast-mode = "sync"' > ~/.titan/config/client.toml
+systemctl start titand
