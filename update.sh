cc8edb2f5e4ec56e489a6ef6e565a440778144dd
MictoNode <94179917+MictoNode@users.noreply.github.com>
2024-12-07 12:39:56 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,21 +6,40 @@ cd ~ || exit
 # Stop old titan program
 sudo systemctl stop titan
 
+# edit by MictoNode :D
+rm -rf titan-chain
+sleep 5
+rm -rf titand
+wget -P ~/. https://github.com/Titannet-dao/titan-chain/releases/download/v0.3.0/titand_0.3.0-1_g167b7fd6.tar.gz
+sleep 5
+tar -zxvf ~/titand_0.3.0-1_g167b7fd6.tar.gz  --strip-components=1
+sleep 5
+mv /root/titand /root/.titan/cosmovisor/genesis/bin/
+sleep 5
+sudo ln -sfn $HOME/.titan/cosmovisor/genesis $HOME/.titan/cosmovisor/current
+sleep 5
+sudo ln -sfn $HOME/.titan/cosmovisor/current/bin/titand /usr/local/bin/titand
+sleep 5
+
 # Back up the .titan directory. If something goes wrong in the middle, you can use the backup to restore and then execute again.
-mv ~/.titan ~/titan_bak_08_08_02
+mv ~/.titan ~/titan_bak_test_3
 
 # Copy directory and other information to the new path
-rsync -av --exclude "data" ~/titan_bak_08_08_02/* ~/.titan
+rsync -av --exclude "data" ~/titan_bak_test_3/* ~/.titan
 
-wget -P ~/. https://raw.githubusercontent.com/Titannet-dao/titan-chain/main/addrbook/addrbook.json
-
-mv ~/addrbook.json ~/.titan/config/addrbook.json
+rm ~/genesis.json
 
 # Download new genesis file
-wget -P ~/. https://raw.githubusercontent.com/Titannet-dao/titan-chain/main/genesis/genesis.json
+wget -P ~/. https://github.com/Titannet-dao/titan-chain/releases/download/v0.3.0/genesis.json
 
 # Replace new genesis file
 mv ~/genesis.json ~/.titan/config/genesis.json
+
+wget -P ~/. https://github.com/Titannet-dao/titan-chain/releases/download/v0.3.0/libwasmvm.x86_64.so
+
+mv ~/libwasmvm.x86_64.so /usr/local/lib/libwasmvm.x86_64.so
+
+sudo ldconfig
 
 mkdir ~/.titan/data
 # Build data/priv_validator_state.json 文件
@@ -29,24 +48,6 @@ echo '{
   "round": 0,
   "step": 0
 }' > ~/.titan/data/priv_validator_state.json
-
-rm -rf titan-chain
-git clone https://github.com/Titannet-dao/titan-chain.git
-sleep 5
-cd titan-chain
-git fetch origin
-sleep 5
-git checkout origin/main
-sleep 5
-go build ./cmd/titand
-sleep 5
-mv /root/titan-chain/titand /root/.titan/cosmovisor/genesis/bin/
-sleep 5
-sudo ln -sfn $HOME/.titan/cosmovisor/genesis $HOME/.titan/cosmovisor/current
-sleep 5
-sudo ln -sfn $HOME/.titan/cosmovisor/current/bin/titand /usr/local/bin/titand
-sleep 5
-sudo systemctl daemon-reload
 
 # Update config/client.toml chain-id
 echo '# This is a TOML config file.
@@ -57,11 +58,11 @@ echo '# This is a TOML config file.
 ###############################################################################
 
 # The network chain ID
-chain-id = "titan-test-3"
+chain-id = "titan-test-4"
 # The keyring s backend, where the keys are stored (os|file|kwallet|pass|test|memory)
 keyring-backend = "os"
 # CLI output format (text|json)
-output = "text"
+output = "json"
 # <host>:<port> to Tendermint RPC interface for this chain
 node = "tcp://localhost:29657"
 # Transaction broadcasting mode (sync|async)
